15c39464a9fae68d03fd673eec35f60c43f6e5b7
ccans <55026208+ccans@users.noreply.github.com>
2020-04-20 17:21:06 -0500

    gamers

---
+++ index.js
@@ -0,0 +1,27 @@
+var droppedDown = false;
+
+function dropDown() {
+  if(droppedDown == false) {
+        $('.dropdown-navbar').stop();
+          curHeight =  $('.dropdown-navbar').height();
+          autoHeight = $(window).height(); - $('.header').height();
+        $('.dropdown-navbar').height(curHeight).animate({height: autoHeight}, 750);
+        $('body').addClass("unscroll");
+        $('.dropdown-navbar').show();
+        $('.dropdown-content').css("display", "block");
+        $('.header').addClass('dropdown');
+        droppedDown = true;
+    } else {
+      $('.dropdown-navbar').stop();
+      curHeight = $('.dropdown-navbar').height();
+      autoHeight = '0px';
+      $('.dropdown-navbar').height(curHeight).animate({height: autoHeight}, 750);
+      $('body').removeClass("unscroll");
+      setTimeout(function(){
+      $('.dropdown-content').css("display", "none");
+    }, 750);
+      droppedDown = false;
+      $('.header').removeClass("dropdown")
+
+    }
+}
