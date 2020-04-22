9ed45fababc5d8648a7b33a5bf42c09c4cbfa1bb
ccans <55026208+ccans@users.noreply.github.com>
2020-04-21 22:25:31 -0500

    gamers

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
 var droppedDown = false;
 
+
+
+// $(".boi").onClick(doStuff);
+
+function clickTime(x) {
+  var pos = x+1;
+  $("tr").find('td:not(:eq(0))').hide();
+  $('td:nth-child('+pos+')').css('display','table-cell');
+  $("tr").find('th:not(:eq(0))').hide();
+  $('li').removeClass('active');
+  $(this).addClass('active');
+}
+
+// Initialize the media query
+  var mediaQuery = window.matchMedia('(min-width: 640px)');
+
+  // Add a listen event
+  mediaQuery.addListener(doSomething);
+
+  // Function to do something with the media query
+  function doSomething(mediaQuery) {
+    if (mediaQuery.matches) {
+      $('.sep').attr('colspan',4);
+    } else {
+      $('.sep').attr('colspan',2);
+    }
+  }
+
+  // On load
+  doSomething(mediaQuery);
+
+
 function dropDown() {
   if(droppedDown == false) {
         $('.dropdown-navbar').stop();
@@ -37,30 +69,3 @@ window.onresize = function(){
   var height = $('.img-top').height();
     $(".main").css("top", height);
 };
-
-$( "ul" ).on( "click", "li", function() {
-  var pos = $(this).index()+2;
-  $("tr").find('td:not(:eq(0))').hide();
-  $('td:nth-child('+pos+')').css('display','table-cell');
-  $("tr").find('th:not(:eq(0))').hide();
-  $('li').removeClass('active');
-  $(this).addClass('active');
-});
-
-// Initialize the media query
-  var mediaQuery = window.matchMedia('(min-width: 640px)');
-
-  // Add a listen event
-  mediaQuery.addListener(doSomething);
-
-  // Function to do something with the media query
-  function doSomething(mediaQuery) {
-    if (mediaQuery.matches) {
-      $('.sep').attr('colspan',5);
-    } else {
-      $('.sep').attr('colspan',2);
-    }
-  }
-
-  // On load
-  doSomething(mediaQuery);
