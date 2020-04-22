c03fa0c1feb9f5ad318892c7d40a0b642a9bb420
ccans <55026208+ccans@users.noreply.github.com>
2020-04-21 22:28:49 -0500

    gucci gang

--- index.js
+++ index.js
@@ -13,23 +13,6 @@ function clickTime(x) {
   $(this).addClass('active');
 }
 
-// Initialize the media query
-  var mediaQuery = window.matchMedia('(min-width: 640px)');
-
-  // Add a listen event
-  mediaQuery.addListener(doSomething);
-
-  // Function to do something with the media query
-  function doSomething(mediaQuery) {
-    if (mediaQuery.matches) {
-      $('.sep').attr('colspan',4);
-    } else {
-      $('.sep').attr('colspan',2);
-    }
-  }
-
-  // On load
-  doSomething(mediaQuery);
 
 
 function dropDown() {
